5b578e4ea1a3995e60f4917d83ffa065f10fd4bf
Mohammed Rashid <rashid@aebiss.com>
2017-06-28 02:29:18 +0400

    New Additions =============
    1. Added disconnect function
    2. Added option for the connection to be one-time or continuous.

--- index.js
+++ index.js
@@ -1,54 +1,101 @@
 'use strict';
 var mqtt=require('mqtt');
-var mqtt_client;
 //Events
 var EventEmitter = require('events');
-//Where topics are stored
-var topicList=[];
 
 class resmetry extends EventEmitter{
-  constructor(){
-    super();
-    this.setMaxListeners(0)
-  }
 
-  connect(host,settings){
-    var currClass=this;
-    mqtt_client= mqtt.connect(host,settings);
-    mqtt_client.on('connect',function(){
-      currClass.emit('connect','Connected');
-    });
-    mqtt_client.on('message',function(topic,message){
-      currClass.emit('message',topic,message.toString());
-      var index=topicList.indexOf(topic);
-      if(index!=-1)
-      {
-        currClass.emit(topic,message.toString());
-        mqtt_client.unsubscribe(topic);
-        topicList.splice(index, 1);
-      }
-    });
-  }
+    /**
+     * Constructor. Initializes listeners and topic array
+     * @param host: MQTT server URL
+     * @param settings: MQTT Server additional settings if any
+     * @param connectionType: 1 or true for a continuous connection, 0 or false for one-time connection
+     */
+    constructor(host,settings,connectionType){
+        //noinspection JSAnnotator
+        super();
+        this.setMaxListeners(0);
+        //Where topics are stored
+        this.topicList=[];
 
-  getMQTTClient(){
-    return mqtt_client;
-  }
+        this.settings=settings;
+        this.connectionType=connectionType;
+        this.host=host;
+        if(connectionType===true){
+            this.connectClient();
+        }
+    }
 
-  //Request
-  request(topic,data,options,responseTopic,callback){
-    var currClass=this;
-    mqtt_client.publish(topic,data,options,function(err){
-      if(err)
-        callback('null');
-      else{
-        mqtt_client.subscribe(responseTopic);
-        topicList.push(responseTopic);
-        currClass.once(responseTopic, function(response){
-          callback('null',response);
+    /**
+     * Function which makes the client to connect to the MQTT server provided and listens for the messages
+     */
+    connectClient(){
+        var self=this;
+        this.mqttClient= mqtt.connect(this.host,this.settings);
+        this.mqttClient.on('connect',function(){
+            self.emit('connect','Connected');
         });
-      }
-    });
-  }
+        this.mqttClient.on('message',function(topic,message){
+            self.emit('message',topic,message.toString());
+            var index=self.topicList.indexOf(topic);
+            if(index!==-1)
+            {
+                self.emit(topic,message.toString());
+                self.mqttClient.unsubscribe(topic);
+                self.topicList.splice(index, 1);
+            }
+        });
+    }
+
+    /**
+     * Disconnects the MQTT client
+     */
+    disconnect(){
+        if(this.mqttClient===undefined){
+            throw "MQTT Client is undefined. Either connection is ONE-TIME type or some issues with the connection";
+        }
+        this.mqttClient.end();
+    }
+
+    /**
+     * Function that returns the mqtt client
+     * @returns the MQTT client
+     */
+    getMQTTClient(){
+        if(this.mqttClient===undefined){
+            throw "MQTT Client is undefined. Either connection is ONE-TIME type or some issues with the connection";
+        }
+        return this.mqttClient;
+    }
+
+    /**
+     * Function that emulates request-response action.
+     * @param topic : The topic you are sending the data to
+     * @param data : The data to be passed in the specified topic
+     * @param options : Any additional MQTT publish options such as QoS
+     * @param responseTopic : Topic to which the response will be sent to
+     * @param callback : The function to which the result will be passed to
+     */
+    request(topic,data,options,responseTopic,callback){
+        var self=this;
+        if(this.connectionType===false){
+            this.connectClient();
+        }
+        this.mqttClient.publish(topic,data,options,function(err){
+            if(err)
+                callback(err);
+            else{
+                self.mqttClient.subscribe(responseTopic);
+                self.topicList.push(responseTopic);
+                self.once(responseTopic, function(response){
+                    callback(null,response);
+                    if(self.connectionType===false)
+                        self.disconnect();
+                });
+            }
+        });
+    }
+
 }
 
 module.exports=resmetry;
--- test.js
+++ test.js
@@ -5,23 +5,31 @@ var settings={
   protocolId: 'MQIsdp',
   protocolVersion: 3
 };
-var resmetry= new resmetrylib();
-//Connect to mqtt server with given credentials
-resmetry.connect(host,settings);
-//Get mqtt client for advanced operations, Refer npm package mqtt for more information
-//For other features offered by MQTT, the modifications can be made using the object
-var mqtt=resmetry.getMQTTClient();
+/**
+ * The last parameter should be true for an always active connection, false for a one time connection
+ * @type {resmetry}
+ */
+var resmetry= new resmetrylib(host,settings,false);
+
+/*
+* Get mqtt client for advanced operations, Refer npm package mqtt for more information
+* For other features offered by MQTT, the modifications can be made using the object
+* Only applicable true is passed the last parameter of the constructor
+*/
+//var mqtt=resmetry.getMQTTClient();
 //MQTT operations
-mqtt.subscribe('request/1');
+//mqtt.subscribe('request/1');
+
 //Connection listener
 resmetry.on('connect',function(message){
   console.log(message);
 });
+
 //Message event listener
 var temp=25;
 resmetry.on('message',function(topic,message){
   console.log("Topic: "+topic,"Message: "+message);
-  if(topic=='request/1'&&message=='temp'){
+  if(topic==='request/1'&&message==='temp'){
     mqtt.publish('response/1',temp+'',{qos:2});
   }
 });
